0ef756d08c3110c1cd2e85c96543d2020f09cbc1
Keks <keks@htmlacademy.ru>
2024-08-10 23:28:25 +0300

    :heavy_check_mark: Сборка #14

---
+++ 14/js/upload-form.js
@@ -0,0 +1,95 @@
+import {destroyFilters, initFilters} from './filters.js';
+const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const ErrorText = {
+  INVALID_COUNT: 'Максимум 5 хештегов',
+  NOT_UNIQUE: 'Хештеги не должны повторяться',
+  INVALID_PATTERN: 'Неправильный хэштег',
+};
+
+const body = document.querySelector('body');
+const form = document.querySelector('.img-upload__form');
+const overlay = document.querySelector('.img-upload__overlay');
+const cancelButton = document.querySelector('.img-upload__cancel');
+const fileField = document.querySelector('.img-upload__input');
+const hashtagField = document.querySelector('.text__hashtags');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error'
+});
+
+const showModal = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown',onDocumentKeydown);
+  initFilters();
+};
+
+const hideModal = () => {
+  form.reset();
+  pristine.reset();
+  overlay.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown',onDocumentKeydown);
+  destroyFilters();
+};
+
+const onFileInputChange = () => {
+  showModal();
+};
+
+const onCancelButtonClick = () => {
+  hideModal();
+};
+const normalizeTags = (string) => string.split(' ').map((tag) => tag.trim()).filter((tag) => tag);
+
+const hasValidTags = (value) => normalizeTags(value).every((tag) => VALID_SYMBOLS.test(tag));
+
+const hasValidCount = (value) => {
+  const array = normalizeTags(value);
+  return array.length <= 5;
+};
+
+const hasUniqueTags = (value) => {
+  const lowerCaseTags = normalizeTags(value).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
+
+const onFormSubmit = (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+};
+
+function onDocumentKeydown (evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideModal();
+  }
+}
+pristine.addValidator(
+  hashtagField,
+  hasValidCount,
+  ErrorText.INVALID_COUNT,
+  3,
+  true
+);
+
+pristine.addValidator(
+  hashtagField,
+  hasUniqueTags,
+  ErrorText.NOT_UNIQUE,
+  2,
+  true
+);
+pristine.addValidator(
+  hashtagField,
+  hasValidTags,
+  ErrorText.INVALID_PATTERN,
+  1,
+  true
+);
+
+fileField.addEventListener('change', onFileInputChange);
+cancelButton.addEventListener('click', onCancelButtonClick);
+form.addEventListener('submit', onFormSubmit);
